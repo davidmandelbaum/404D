1d1813fdb076b07d4214fcb137a592c08c7be203
David Mandelbaum <david.m.mandelbaum@gmail.com>
2014-11-10 02:13:37 -0500

    added capno_helpers

---
+++ py/capno_helpers.py
@@ -0,0 +1,36 @@
+from __future__ import division
+import sys
+import numpy as np
+from math import *
+
+# CAPNOGRAPHY ALGORITHM VARIABLES (adjustable)
+# --------------------------------------------
+
+# Depth
+min_acceptable_depth = 4
+max_acceptable_depth = 5
+
+def max_alg(depth):
+    depth = depth/10
+    if min_acceptable_depth < depth and depth < max_acceptable_depth:
+        return 2.5
+    elif depth > max_acceptable_depth:
+        return (1-(10*depth-5))
+    else:
+        return (2.5 - (2 - (0.25*sqrt(depth))))
+
+def min_alg(depth):
+    depth = depth/10
+    if depth == 0:
+        return 1
+    else:
+        return 1-depth
+
+def time_alg(current, last):
+    rate = (current - last)/100
+    if 0.5 < rate and rate < 0.6:
+        return 1
+    elif 0.3 < rate and rate < 0.8:
+        return (1 - (2*abs(0.55-rate)))
+    else:
+        return 0
