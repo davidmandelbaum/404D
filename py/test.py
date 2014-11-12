02bdfe1bb300a6d2f95ea2aea2acb2681a76fc3c
David Mandelbaum <david.m.mandelbaum@gmail.com>
2014-11-12 14:34:30 -0500

    updated test.py to just send status

--- py/test.py
+++ py/test.py
@@ -3,21 +3,21 @@ import sys
 import json
 from time import sleep
 
-# status = {}
-# status['time'] = 5
-# status['depth'] = 4
-# status['rate'] = 110
-# status['capno'] = 1000
-# status_out = json.dumps({ "status_msg": status})
-# sys.stdout.write(status_out + "\n")
+status = {}
+status['time'] = 5
+status['depth'] = 4
+status['rate'] = 110
+status['capno'] = 1000
+status_out = json.dumps({ "status_msg": status})
+sys.stdout.write(status_out + "\n")
 
-nums = range(0, 300, 1)
-nums = [x/10 for x in nums]
-for n in nums:
-    data_point = []
-    data_point.append((n, round((n%1), 2)))
-    data_point_out = json.dumps({ "data_point": data_point })
-    sys.stdout.write(data_point_out + "\n")
+# nums = range(0, 300, 1)
+# nums = [x/10 for x in nums]
+# for n in nums:
+#     data_point = []
+#     data_point.append((n, round((n%1), 2)))
+#     data_point_out = json.dumps({ "data_point": data_point })
+#     sys.stdout.write(data_point_out + "\n")
 
 sys.stdout.write("end!")
 
