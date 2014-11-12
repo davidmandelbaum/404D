434c71cabc3710b3c3c4671cf61a6d192e35f4a8
David Mandelbaum <david.m.mandelbaum@gmail.com>
2014-11-12 13:28:09 -0500

    updated index.jade for status_msg instead of status

--- py/test.py
+++ py/test.py
@@ -1,20 +1,16 @@
 import sys
 import json
 
-data_points = []
-data_points.append((1, 2))
-data_points.append((2, 3))
-
-status = json.dumps({ "data_points": data_points})
-sys.stdout.write(status + "\n")
-
-status = []
-status.append((1, 2))
-status_out = json.dumps({ "status_msg": status})
+status = {}
+status['time'] = 5
+status['depth'] = 4
+status['rate'] = 110
+status['capno'] = 1000
+status_out = json.dumps({ "status_msg": json.dumps(status)})
 sys.stdout.write(status_out + "\n")
 
 final_stats = {}
 final_stats['time'] = 100
 final_stats['depth'] = 20
 final_stats_out = json.dumps({ "final_stats": json.dumps(final_stats) })
-sys.stdout.write(final_stats_out + "\n")
+# sys.stdout.write(final_stats_out + "\n")
