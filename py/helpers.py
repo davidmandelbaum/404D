5c652a6eb853d56a3161ad6b3fcad5d9d2da5100
David Mandelbaum <david.m.mandelbaum@gmail.com>
2014-11-13 16:42:19 -0500

    updated all files to use extrema function

--- py/helpers.py
+++ py/helpers.py
@@ -1,5 +1,6 @@
 from __future__ import division
 from scipy import signal as sig
+from extrema import extrema
 import numpy as np
 
 def calc_offset(data):
@@ -15,7 +16,7 @@ def calc_comp_rate(y_vals, s):
     if s > 0:
         if len(y_vals) > 0:
             y = np.array(y_vals)
-            max_extrema = sig.argrelextrema(y, np.greater, 0)[0]
+            max_extrema = extrema(y, True, False)[0]
             compressions = max_extrema.size
             rate = compressions/s*60
         else:
@@ -27,7 +28,7 @@ def calc_comp_depth(y_vals, s, data, window_vals):
         if len(y_vals) > 0:
             depths = []
             y = np.array(y_vals)
-            max_extrema = sig.argrelextrema(y, np.greater, 0)[0]
+            max_extrema = extrema(y, True, False)[0]
             off = window_vals[0][0]
             for x in max_extrema:
                 depths.append(window_vals[x][1])
@@ -47,7 +48,7 @@ def final_stats(y_vals, time_limit, data):
         oned_yvals.append(y[1])
     y_vals = np.array(oned_yvals)
 
-    max_extrema = sig.argrelextrema(y_vals, np.greater, 0)[0]
+    max_extrema = extrema(y_vals, True, False)[0]
 
     compressions = max_extrema.size
 
