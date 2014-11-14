cbff828d4f433e055c647336e6c90f93b3debc38
David Mandelbaum <david.m.mandelbaum@gmail.com>
2014-11-14 16:35:08 -0500

    added metricsgraphics

---
+++ public/javascripts/index2.js
@@ -0,0 +1,30 @@
+var socket = io();
+var compressions = [];
+socket.on('data_point', function(data_point) {
+  console.log(data_point);
+  compressions.push({"time": parseInt(data_point[0][0]), "depth": parseInt(data_point.depth[0][1])});
+  console.log(compressions);
+  data_graphic({
+    title: "Compressions",
+      data: compressions,
+      target: "#compressions",
+      x_accessor: 'time',
+      y_accessor: 'depth'
+  });
+});
+socket.on('data_points', function(data_points) {
+  console.log("data points received");
+  var i = 0;
+  points = JSON.parse(data_points.points);
+  for (i = 0; i < points.length; i++){
+    compressions.push({"time": parseFloat(points[i][0]), "depth": parseFloat(points[i][1])});
+  }
+  console.log(compressions);
+  data_graphic({
+    title: "Compressions",
+    data: compressions,
+    target: "#compressions",
+    x_accessor: 'time',
+    y_accessor: 'depth'
+  });
+});
