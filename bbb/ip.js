857243a92e293c51e1c8af7d9cdd1f37101d6230
David Mandelbaum <david.m.mandelbaum@gmail.com>
2014-12-01 14:29:15 -0500

    set timeout on ip script

--- bbb/ip.js
+++ bbb/ip.js
@@ -1,8 +1,11 @@
 var exec = require('child_process').exec;
 var request = require('request');
 
-var output = exec('ifconfig | grep "inet addr" | head -1', function(err, stdout) {
-  request.post(
-    'http://meng404d.herokuapp.com/addr',
-    { form : { addr: stdout } });
-});
+setTimeout(function() {
+  var output = exec('ifconfig | grep \'inet addr\' | head -1', function(err, stdout) {
+    console.log(stdout);
+    request.post(
+      'http://meng404d.herokuapp.com/addr',
+      { form : { addr: stdout } });
+  });
+}, 10000);
