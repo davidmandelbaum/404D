5771d2548819450fcfd0513055a7d54500427306
David Mandelbaum <david.m.mandelbaum@gmail.com>
2014-12-03 12:04:50 -0500

    switchd constant to 1

--- py/pipeplot.py
+++ py/pipeplot.py
@@ -40,7 +40,7 @@ try:
     fallrate = 18
     goodrise = 0.3 # everything but shallow makes sense at -0.2, shallow makes sense at 0.16
     badrise = 0.6*goodrise # should still allow for rescue below 10mmHg w/ perfect CPR
-    constant = 50
+    constant = 1
     maxscore = 1250
     deathscore = 1250
     neardeath = deathscore + ((maxscore-deathscore)/4)
