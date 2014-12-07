32a9872d4e686b952e6107b55a72ba17e3511698
David Mandelbaum <david.m.mandelbaum@gmail.com>
2014-12-07 12:11:25 -0500

    updated compression conversion

--- py/pipeplot.py
+++ py/pipeplot.py
@@ -22,7 +22,7 @@ try:
     comp_depths = []
     nums = np.arange(0, time_limit, 10)
     old_num = 0
-    conversion = 11.09
+    conversion = 7.59
     ylim = 7
     comp_status = 0
     depth_status = 0
