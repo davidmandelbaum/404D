9b9b0b46c1bbdf9d5e22b8a24cb36cf743aabd3c
David Mandelbaum <david.m.mandelbaum@gmail.com>
2014-12-09 14:49:27 -0500

    different interpolation

--- public/javascripts/index2.js
+++ public/javascripts/index2.js
@@ -40,7 +40,7 @@ socket.on('begin', function(time) {
     min_y: -7,
     max_y: 0,
     area: false,
-    interpolate: "linear",
+    interpolate: "basis",
     x_label: "Time (s)",
     y_label: "Depth (cm)",
     baselines: baselines
@@ -104,7 +104,7 @@ socket.on('data_points', function(data_points) {
     min_y: -7,
     max_y: 0,
     area: false,
-    interpolate: "linear",
+    interpolate: "basis",
     x_label: "Time (s)",
     y_label: "Depth (cm)",
     baselines: baselines
@@ -227,7 +227,7 @@ socket.on('final_stats', function(final_stats) {
     min_y: -7,
     max_y: 0,
     area: false,
-    interpolate: "linear",
+    interpolate: "basis",
     x_label: "Time (s)",
     y_label: "Depth (cm)",
     baselines: baselines
